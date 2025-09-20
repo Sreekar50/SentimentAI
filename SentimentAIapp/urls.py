e98fbdf74d573c31d0d9ec75b2bf38447dcbb2dd
Sreekar reddy <134253732+Sreekar50@users.noreply.github.com>
2025-09-20 20:29:09 +0530

    Update urls.py

--- SentimentAIapp/urls.py
+++ SentimentAIapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import fetch_comments
-from .auth import register_user, login_user, logout_user
+from .views import fetch_comments, get_analysis_history
+from .auth import register_user, login_user, logout_user, check_auth_status
 
 urlpatterns = [
     path('fetch_comments/', fetch_comments, name='fetch_comments'),
-    path('register/', register_user),
-    path('login/', login_user),
-    path('logout/', logout_user),
+    path('history/', get_analysis_history, name='get_analysis_history'),
+    
+    path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('auth-status/', check_auth_status, name='check_auth_status'),
 ]
-
